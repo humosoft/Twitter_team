be7a44f45adc29e08a9d3ad8212180c9932650b9
Shoha-N1 <abduvohidovs85@gmail.com>
2023-04-05 16:40:41 +0500

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './compnents/login_page/Login'
 
+
 function App() {
   return (
     <div className="App">
-      <Login />
+    <Login />
     </div>
   )
 }
---
+++ src/compnents/login_page/input_log1/Input_1.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Input.scss"
+
+export default function Input_1() {
+  return (
+    <div>Input_1</div>
+  )
+}
